3a8ed428e1ca6e6613e342867a42e36ce0033ec8
Bekir Samet Şirinel <sametsirinel@gmail.com>
2025-09-19 12:33:32 +0300

    added model releations

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Article extends Model
 {
     use HasUuids;
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
 
 }
--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
     use HasUuids;
+
+    public function article(): BelongsTo
+    {
+        return $this->belongsTo(Article::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
