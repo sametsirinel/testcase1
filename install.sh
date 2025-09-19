a6e859b750ab08bf6c04b3d3cadfd6d2698b11cf
Bekir Samet Şirinel <sametsirinel@gmail.com>
2025-09-19 21:29:06 +0300

    added install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cp .env.example .env
+
+docker run --rm \
+    -u "$(id -u):$(id -g)" \
+    -v "$(pwd):/var/www/html" \
+    -w /var/www/html \
+    laravelsail/php84-composer:latest \
+    composer install --ignore-platform-reqs
+
+alias sail='./vendor/bin/sail '
+
+sail up -d
+
